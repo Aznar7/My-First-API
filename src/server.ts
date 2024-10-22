e0d1c02966c6c7732d576e6c1bfd2924bf208369
Aznar7 <paulaznartequen@gmail.com>
2024-10-22 23:41:10 +0200

    AAAAAAAAAAAA

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,39 @@
 import './loadEnvironment.js';
 import express from 'express';
 
-// Initialize express
+// Inicializar express
 const app = express();
 const port = process.env.HOST_PORT ?? '3000';
 
-app.get('/ping', (req, res) => res.send('pong'));
+// Datos: Nombres de los integrantes de la clase
+const alumnos = [
+    'Nil',
+    'Joshua',
+    'Oriol',
+    'Saten',
+    'Adri',
+    'Eisnar 🐐',
+    'Ocsar',
+    '& Company'
+];
 
-app.listen(process.env.HOST_PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+// Ruta de ping (para comprobar que el servidor está en marcha)
+app.get('/ping', (req, res) => res.send('pongk'));
+
+// Ruta de ping (para comprobar que el servidor está en marcha)
+app.get('/test', (req, res) => res.send('aa'));
+
+// Ruta de la API que devuelve el JSON con los alumnos
+app.get('/alumnos', (req, res) => {
+  res.json({
+    totalAlumnos: alumnos.length,
+    nombresAlumnos: alumnos
+  });
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
 
 export default app;
